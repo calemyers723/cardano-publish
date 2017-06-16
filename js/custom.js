ef83e4f170ec4a0ed5f0fde12e9eee3bcce72b47
calemyers723 <cale.myers723@gmail.com>
2017-06-16 21:58:14 +0800

    language menu

--- js/custom.js
+++ js/custom.js
@@ -10,5 +10,25 @@ jQuery(document).ready(function() {
       _this.parent().find('p').text(_this.parent().find('.short-desc').val());
       _this.text(jQuery('#read_more_str').val());
     }
+  });
+
+  var language = jQuery('#language').val();
+  if (language == 'ja') {
+    jQuery('.select-language').val('日本語');
+  }
+  jQuery('.select-language').selectpicker();
+
+  jQuery('select.select-language').on('change', function() {
+    console.log(jQuery(this).val());
+    var language = jQuery(this).val();
+    var defaultUrl = jQuery('#default-url').val();
+    if (language == 'English') {
+      // jQuery('#en-url').trigger('click');
+      window.location.href = window.location.origin + defaultUrl;
+    } else if (language == '日本語') {
+      // jQuery('#ja-url').trigger('click');
+      window.location.href = window.location.origin + '/ja' + defaultUrl;
+    }
+
   })
 })
